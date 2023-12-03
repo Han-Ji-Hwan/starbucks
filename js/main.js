8aa1fa5946633fe61cc6faf6fcfe9be53200e90e
Han-Ji-Hwan <gkswlghsk100@naver.com>
2023-12-03 13:45:23 +0900

    공통 모듈 분리

--- js/main.js
+++ js/main.js
@@ -1,20 +1,3 @@
-const searchEl = document.querySelector('.search');
-const searchInputEl = searchEl.querySelector('input');
-
-searchEl.addEventListener('click', function () {
-  searchInputEl.focus();
-});
-
-searchInputEl.addEventListener('focus', function () {
-  searchEl.classList.add('focused');
-  searchInputEl.setAttribute('placeholder', '통합검색');
-});
-
-searchInputEl.addEventListener('blur', function () {
-  searchEl.classList.remove('focused');
-  searchInputEl.setAttribute('placeholder', '');
-});
-
 const badgeEl = document.querySelector('header .badges');
 const toTopEl = document.querySelector('#to-top');
 
@@ -45,7 +28,6 @@ window.addEventListener('scroll', _.throttle(function () {
   }
 }, 300)); // _.throttle(함수, 시간)
 
-// 최상단 버튼 이동 오류
 toTopEl.addEventListener('click', function() {
   gsap.to(window, .7, { 
     scrollTo: 0
@@ -142,6 +124,3 @@ spyEls.forEach(function(spyEl){
     .setClassToggle(spyEl, 'show')
     .addTo(new ScrollMagic.Controller());
 });
-
-const thisYear = document.querySelector('.this-year');
-thisYear.textContent = new Date().getFullYear();
